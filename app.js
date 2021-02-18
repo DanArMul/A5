3a1fb17cfeb10f1f5335f31f9b2c6ce9be4d1666
Daniel Muljono <dmuljono@ucsd.edu>
2021-02-18 23:43:48 +0700

    Node app.js running

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ var express = require('express');
 var app = express();
 
 // Print logs to the console and compress pages we send
-app.use(express.logger());
-app.use(express.compress());
+//app.use(express.logger());
+//app.use(express.compress());
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
 app.use(express.static(__dirname + '/static'));
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/static'));
+});
 // Start the server
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
